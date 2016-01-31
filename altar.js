b13ae82c4b3b238407f4f08d6432bafcf6c0fe86
unknown <tyler0310@gmail.com>
2016-01-31 13:05:05 -0800

    Adding altar animation

--- altar.js
+++ altar.js
@@ -6,35 +6,34 @@
 
   function preload(game) {
     console.log('altar preload');
-    game.load.image('altar', 'assets/altar.png');
-    game.load.image('star', 'assets/star.png');
+    game.load.atlasJSONHash('altar', 'assets/dej_animations.png', 'assets/dej_animations.json');
 
   }
 
   function create(game) {
-    function tweenTint(obj, startColor, endColor, time) {
-      // create an object to tween with our step value at 0
-      var colorBlend = {step: 0};
-      // create the tween on this object and tween its step property to 100
-      var colorTween = game.add.tween(colorBlend).to({step: 100}, time, Phaser.Easing.Linear.None, true, 0, -1, true );
-      // run the interpolateColor function every time the tween updates, feeding it the
-      // updated value of our tween each time, and set the result as our tint
-      colorTween.onUpdateCallback(function() {
-        obj.tint = Phaser.Color.interpolateColor(startColor, endColor, 100, colorBlend.step);
-      });        // set the object to the start color straight away
-      obj.tint = startColor;
-      // start the tween
-      colorTween.start();
-    }
+    // function tweenTint(obj, startColor, endColor, time) {
+    //   // create an object to tween with our step value at 0
+    //   var colorBlend = {step: 0};
+    //   // create the tween on this object and tween its step property to 100
+    //   var colorTween = game.add.tween(colorBlend).to({step: 100}, time, Phaser.Easing.Linear.None, true, 0, -1, true );
+    //   // run the interpolateColor function every time the tween updates, feeding it the
+    //   // updated value of our tween each time, and set the result as our tint
+    //   colorTween.onUpdateCallback(function() {
+    //     obj.tint = Phaser.Color.interpolateColor(startColor, endColor, 100, colorBlend.step);
+    //   });        // set the object to the start color straight away
+    //   obj.tint = startColor;
+    //   // start the tween
+    //   colorTween.start();
+    // }
     console.log('altar create');
     //300x150
-    altar = game.add.sprite(game.world.centerX, 300, 'altar');
-    altar.anchor.setTo(0.5, 0.5);
-    altar.scale.setTo(2,2);
-    star = game.make.sprite(0, 0, 'star');
-    star.anchor.setTo(0.5, 0.5);
-    altar.addChild(star);
-    tweenTint(star, 0xCC4518, 0x2E1006);
+    altar = game.add.sprite(825, 250, 'anim', 'altar_animation/altar_animation0001.png');
+    altar.animations.add('glow', Phaser.Animation.generateFrameNames('altar_animation/altar_animation', 1, 78, '.png', 4), 19, true, false);
+    altar.animations.play('glow');
+    altar.scale.setTo(0.5,0.5);
+    // altar.anchor.setTo(0.5, 0.5);
+    // altar.scale.setTo(2,2);
+    // tweenTint(star, 0xCC4518, 0x2E1006);
   }
 
   function update(game) {
