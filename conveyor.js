fd2892e47b8dcc7017c828d54b86add0592db647
unknown <tyler0310@gmail.com>
2016-01-31 13:07:08 -0800

    Re-adding worker animations and music

--- conveyor.js
+++ conveyor.js
@@ -6,7 +6,7 @@
 
   function preload(game) {
     game.load.image('conveyor', 'assets/conveyor.png');
-    game.load.image('worker', 'assets/worker.png');
+    game.load.atlasJSONHash('anim', 'assets/dej_animations.png', 'assets/dej_animations.json');
   }
 
   function create(game) {
@@ -18,7 +18,11 @@
       var y = 600 + (i % 3) * 200; 
       var c = game.add.sprite(x, y, 'conveyor');
       c.scale.x *= lefthand? 1 : -1;
-      var w = game.add.sprite(700, -60, 'worker');
+      var w = game.add.sprite(700, -60, 'anim', 'worker_animation/worker_animation0001.png');
+      w.scale.setTo(0.25,0.25);
+
+      w.animations.add('bob', Phaser.Animation.generateFrameNames('worker_animation/worker_animation', 1, 27, '.png', 4), 19, true, false);
+      w.animations.play('bob');
       c.addChild(w);
       conveyors.push(c);
       layers.foreground.add(c);
