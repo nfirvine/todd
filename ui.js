6927103ba88cd252f636943e4c95a21955e29985
Nick Irvine <nirvine@bns-g.com>
2016-01-31 11:14:55 -0800

    Add music

--- ui.js
+++ ui.js
@@ -1,8 +1,4 @@
 (function() {
-  var bomb_hole;
-  var trash_bin;
-  //background is in the ui just cause I'm lazy
-  var background;
   function init(game) {
   }
 
@@ -10,16 +6,18 @@
     game.load.image('bomb_hole', 'assets/bomb_hole.png');
     game.load.image('trash_bin', 'assets/trash_bin.png');
     game.load.image('background', 'assets/background.png');
-
+    game.load.audio('soundtrack', 'assets/music.mp3');
   }
 
   function create(game) {
-    background = game.add.sprite(0,0, 'background');
+    var background = game.add.sprite(0,0, 'background');
     layers.background.add(background);
-    trash_bin = game.add.sprite(500, 1300, 'trash_bin');
+    var trash_bin = game.add.sprite(500, 1300, 'trash_bin');
     layers.ui.add(trash_bin);
-    bomb_hole = game.add.sprite(1400, 1300, 'bomb_hole');
+    var bomb_hole = game.add.sprite(1400, 1300, 'bomb_hole');
     layers.ui.add(bomb_hole);
+    var soundtrack = game.add.audio('soundtrack', 1, true);
+    soundtrack.play();
   }
 
   function update(game) {
