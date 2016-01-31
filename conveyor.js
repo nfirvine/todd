829e172545f95e0f83465fbf80fb920ad71e01c0
Nick Irvine <nirvine@bns-g.com>
2016-01-30 16:12:15 -0800

    Fix up conveyor

--- conveyor.js
+++ conveyor.js
@@ -13,12 +13,12 @@
     for (var i=0; i < 6; i++) {
       var lefthand = i < 3;
       var x = lefthand? 0 : game.world.width;
+      x -= (3 - (i % 3)) * 20 * (lefthand? 1 : -1); 
       var y = 200 + (i % 3) * 125; 
       var c = game.add.sprite(x, y, 'conveyor');
-      c.scale.setTo(0.5, 0.5);
+      c.scale.setTo(0.25, 0.25);
       if (!lefthand) {
         c.scale.x *= -1;
-        c.x = game.world.width;
       }
       conveyors.push(c);
     }
